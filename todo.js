c4854037273badbf3c23815d03cfd3f6889f708a
omar saade <omarsaadeh11@gmail.com>
2022-02-07 22:51:57 +0200

    To-Do List

--- todo.js
+++ todo.js
@@ -1,1 +1,70 @@
-//
+
+let p = document.getElementById("texted");
+
+
+
+function nomo() {
+
+
+
+    if (document.getElementById("text").value == '') {
+
+
+
+        alert("please enter something habibi");
+        setInterval(() => {
+            document.getElementById("text").style.display = "block";
+        }, 1000);
+        document.getElementById("text").style.display = "none";
+
+    } else {
+
+
+
+        document.getElementById("text").style.display = "block";
+
+
+        let text = document.getElementById("text");
+
+
+        const button = document.createElement("button");
+
+        const texto = document.createTextNode("Delete");
+        const p = document.createElement("p");
+
+
+        const textnode = document.createTextNode(text.value);
+
+
+        p.appendChild(textnode);
+        p.appendChild(button);
+
+        button.appendChild(texto);
+
+
+
+        document.getElementById("texted").appendChild(p);
+        document.getElementById("text").value = "";
+        button.setAttribute("class", "helloButton");
+
+        button.addEventListener("click", function () {
+            p.remove();
+
+
+            // });
+
+        })
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
